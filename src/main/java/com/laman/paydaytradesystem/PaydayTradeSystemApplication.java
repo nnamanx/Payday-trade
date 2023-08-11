e74a2c12b402f2c0d4a707c601982feae883cf23
lamankhudadatzada <lkhudadatzada@gmail.com>
2023-08-11 14:09:36 +0400

    "initializing"

---
+++ src/main/java/com/laman/paydaytradesystem/PaydayTradeSystemApplication.java
@@ -0,0 +1,13 @@
+package com.laman.paydaytradesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaydayTradeSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaydayTradeSystemApplication.class, args);
+    }
+
+}
