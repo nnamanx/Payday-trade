a77c3ae5decc7a964535da05ad4ab8ee38740541
lamankhudadatzada <lkhudadatzada@gmail.com>
2023-08-13 19:21:34 +0400

    "Authentication Provider Bean"

--- src/main/java/com/laman/paydaytradesystem/config/ApplicationConfig.java
+++ src/main/java/com/laman/paydaytradesystem/config/ApplicationConfig.java
@@ -4,8 +4,12 @@ import com.laman.paydaytradesystem.repository.CustomerRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import static com.laman.paydaytradesystem.model.constants.Messages.USER_NOT_FOUND;
 
@@ -17,8 +21,25 @@ public class ApplicationConfig {
 
     @Bean
     public UserDetailsService userDetailsService() {
+
         return username -> customerRepository.findByEmail(username)
                 .orElseThrow(() -> new UsernameNotFoundException(USER_NOT_FOUND));
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setUserDetailsService(userDetailsService());
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+        return daoAuthenticationProvider;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+
+
+        return new BCryptPasswordEncoder();
     }
 }
 
