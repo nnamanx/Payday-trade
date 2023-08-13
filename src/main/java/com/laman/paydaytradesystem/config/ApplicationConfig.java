27df8723284844af61dad38737d5dd470c8bd7d9
lamankhudadatzada <lkhudadatzada@gmail.com>
2023-08-13 19:26:17 +0400

    "Authentication Manager Bean"

--- src/main/java/com/laman/paydaytradesystem/config/ApplicationConfig.java
+++ src/main/java/com/laman/paydaytradesystem/config/ApplicationConfig.java
@@ -4,8 +4,10 @@ import com.laman.paydaytradesystem.repository.CustomerRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -40,6 +42,14 @@ public class ApplicationConfig {
 
 
         return new BCryptPasswordEncoder();
+    }
+
+
+    // What is this method gonna do?
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+
+        return authenticationConfiguration.getAuthenticationManager();
     }
 }
 
