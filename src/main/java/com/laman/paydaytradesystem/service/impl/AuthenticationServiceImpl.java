ac23b6807d645e0c4a037d243bf31e22676a6c78
lamankhudadatzada <lkhudadatzada@gmail.com>
2023-08-14 17:40:44 +0400

    "Authentication Service: register method"

--- src/main/java/com/laman/paydaytradesystem/service/impl/AuthenticationServiceImpl.java
+++ src/main/java/com/laman/paydaytradesystem/service/impl/AuthenticationServiceImpl.java
@@ -2,16 +2,39 @@ package com.laman.paydaytradesystem.service.impl;
 
 import com.laman.paydaytradesystem.model.dto.request.CustomerRequestDto;
 import com.laman.paydaytradesystem.model.dto.response.AuthenticationResponseDto;
+import com.laman.paydaytradesystem.model.entity.Customer;
+import com.laman.paydaytradesystem.model.enums.Role;
+import com.laman.paydaytradesystem.repository.CustomerRepository;
 import com.laman.paydaytradesystem.service.AuthenticationService;
+import com.laman.paydaytradesystem.service.JwtService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class AuthenticationServiceImpl implements AuthenticationService {
+
+    private final CustomerRepository customerRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+
     @Override
     public AuthenticationResponseDto register(CustomerRequestDto customerRequestDto) {
-        return null;
+
+        var customer = Customer.builder()
+                .fullName(customerRequestDto.getName())
+                .email(customerRequestDto.getEmail())
+                .password(passwordEncoder.encode(customerRequestDto.getPassword()))
+                .role(Role.USER)    // defining USER as a default
+                .build();
+
+        customerRepository.save(customer);
+
+        var jwtToken = jwtService.generateToken(customer);
+        return AuthenticationResponseDto.builder()
+                .token(jwtToken)
+                .build();
     }
 
     @Override
