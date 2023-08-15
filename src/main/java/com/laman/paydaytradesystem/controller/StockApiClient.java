87a68bdac51ea7a5031237b484d4620cbb7e2df7
lamankhudadatzada <lkhudadatzada@gmail.com>
2023-08-15 14:09:24 +0400

    "Feign Config"

--- src/main/java/com/laman/paydaytradesystem/controller/StockApiClient.java
+++ src/main/java/com/laman/paydaytradesystem/controller/StockApiClient.java
@@ -1,13 +1,14 @@
 package com.laman.paydaytradesystem.controller;
 
 import com.laman.paydaytradesystem.model.entity.Stock;
+import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
 @RequestMapping("/payday/v1/")
-@FeignClient(name = "dummy-json-api", url = "https://dummyapi.io/data/api")
+@FeignClient(url = "https://dummyapi.io/data/api")
 public interface StockApiClient {
 
     @GetMapping("/stocks")
