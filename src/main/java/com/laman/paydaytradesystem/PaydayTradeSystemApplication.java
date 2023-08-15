370b3cd22373bf143c658c30a58e1bb140edd65f
lamankhudadatzada <lkhudadatzada@gmail.com>
2023-08-15 20:31:45 +0400

    "Feign Config"

--- src/main/java/com/laman/paydaytradesystem/PaydayTradeSystemApplication.java
+++ src/main/java/com/laman/paydaytradesystem/PaydayTradeSystemApplication.java
@@ -1,14 +1,22 @@
 package com.laman.paydaytradesystem;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
-public class PaydayTradeSystemApplication  {
-
+@RequiredArgsConstructor
+public class PaydayTradeSystemApplication{ // implements CommandLineRunner
+//    private final StockApiClient stockApiClient;
     public static void main(String[] args) {
         SpringApplication.run(PaydayTradeSystemApplication.class, args);
     }
+
+//    @Override
+//    public void run(String... args) throws Exception {
+//           String s= stockApiClient.getAllMockStocks();
+//        System.out.println(s);
+//    }
 }
--- src/main/java/com/laman/paydaytradesystem/controller/StockApiClient.java
+++ src/main/java/com/laman/paydaytradesystem/controller/StockApiClient.java
@@ -1,13 +1,15 @@
 package com.laman.paydaytradesystem.controller;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 
 //@FeignClient (url = "https://dummyapi.io/data/api")
-@FeignClient(value = "rate", url = "${https://dummyapi.io/data/api}")
+@FeignClient(value = "rate", url = "$https://dummyjson.com/}")
+@Component
 public interface StockApiClient {
 
-    @GetMapping("/stocks")
+    @GetMapping("/products")
     String getAllMockStocks();
 
 }
