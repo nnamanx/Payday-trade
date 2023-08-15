d636733ea65ed52b0a1bd7a1dfc0a80d293d0868
lamankhudadatzada <lkhudadatzada@gmail.com>
2023-08-15 20:32:55 +0400

    "Feign Config"

--- src/main/java/com/laman/paydaytradesystem/controller/StockApiClient.java
+++ src/main/java/com/laman/paydaytradesystem/controller/StockApiClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 
-//@FeignClient (url = "https://dummyapi.io/data/api")
+
 @FeignClient(value = "rate", url = "$https://dummyjson.com/}")
 @Component
 public interface StockApiClient {
