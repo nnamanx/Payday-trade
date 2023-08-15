35d1536499ea464c887be5e35bf4e71d90e33a89
lamankhudadatzada <lkhudadatzada@gmail.com>
2023-08-15 19:36:59 +0400

    "Feign Config"

--- src/main/java/com/laman/paydaytradesystem/PaydayTradeSystemApplication.java
+++ src/main/java/com/laman/paydaytradesystem/PaydayTradeSystemApplication.java
@@ -2,12 +2,13 @@ package com.laman.paydaytradesystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-public class PaydayTradeSystemApplication {
+@EnableFeignClients
+public class PaydayTradeSystemApplication  {
 
     public static void main(String[] args) {
         SpringApplication.run(PaydayTradeSystemApplication.class, args);
     }
-
 }
--- src/main/java/com/laman/paydaytradesystem/controller/StockApiClient.java
+++ src/main/java/com/laman/paydaytradesystem/controller/StockApiClient.java
@@ -1,14 +1,13 @@
 package com.laman.paydaytradesystem.controller;
 
-import com.laman.paydaytradesystem.model.entity.Stock;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.util.List;
-
 //@FeignClient (url = "https://dummyapi.io/data/api")
-//@FeignClient(value = "rate", url = "${https://dummyapi.io/data/api}")
+@FeignClient(value = "rate", url = "${https://dummyapi.io/data/api}")
 public interface StockApiClient {
 
     @GetMapping("/stocks")
-    List<Stock> getAllMockStocks();
+    String getAllMockStocks();
+
 }
