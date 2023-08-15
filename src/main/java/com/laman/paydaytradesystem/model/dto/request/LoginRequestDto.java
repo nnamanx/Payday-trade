a10189d3086fc7c55574c572d12a62af4a3dbd11
lamankhudadatzada <lkhudadatzada@gmail.com>
2023-08-15 13:21:15 +0400

    "Login Request DTO"

---
+++ src/main/java/com/laman/paydaytradesystem/model/dto/request/LoginRequestDto.java
@@ -0,0 +1,26 @@
+package com.laman.paydaytradesystem.model.dto.request;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import static com.laman.paydaytradesystem.model.constants.Messages.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class LoginRequestDto {
+
+    @NotBlank
+    @Email(message = INVALID_EMAIL)
+    String email;
+
+    @NotBlank(message = INVALID_PASSWORD)
+    @Size(min = 6, message = PASSWORD_RULE)
+    String password;
+}
