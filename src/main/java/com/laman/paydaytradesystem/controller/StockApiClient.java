e9f3de44135deabf491ca65f2212813abb31da91
lamankhudadatzada <lkhudadatzada@gmail.com>
2023-08-19 08:23:13 +0400

    "StockApiClient edit"

--- src/main/java/com/laman/paydaytradesystem/controller/StockApiClient.java
+++ src/main/java/com/laman/paydaytradesystem/controller/StockApiClient.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import java.util.List;
 
 
-@FeignClient(value = "rate", url = "https://dummyjson.com/products")
+@FeignClient(value = "StockData", url = "https://dummyjson.com/products")
 @Component
 public interface StockApiClient {
 
